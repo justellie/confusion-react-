4818c89864839944e2434a35a4b1658b6dcea3e3
Elio <elioruiz.en@gmail.com>
2019-01-27 23:19:11 -0400

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -3,9 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';//importando exactamente esos componentes desde reactstrap
 import Menu from './components/MenuComponent';
-
+import {DISHES} from './shared/dishes';
 
 class App extends Component {
+   constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
   render() {
     return (
       <div className="App">
@@ -14,7 +20,7 @@ class App extends Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
-        <Menu />
+        <Menu dishes={this.state.dishes} />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';//importando bootstrap 
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));//Esto significa renderizar el componente App y "pintalo" en donde esta el elemento "root"
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
