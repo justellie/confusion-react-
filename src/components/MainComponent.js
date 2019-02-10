1cce6501d620ea2508e7a03abd319ad335951ee1
Elio <elioruiz.en@gmail.com>
2019-02-09 23:55:40 -0400

    Single Page Applications Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,27 +1,42 @@
 //Este componente es el que se encarga de pasar todos los datos a los demas componentes para que ellos lo muestren por pantalla
 import React, { Component } from 'react';
+import {Switch, Route, Redirect} from 'react-router-dom';
+import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 import Menu from './MenuComponent';
 import DishDetail from './DishdetailComponent';
-import { DISHES } from '../shared/dishes';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
-import {Switch, Route, Redirect} from 'react-router-dom';
+import Contact from './ContactComponent';
+
+
 
 class Main extends Component {
 
-  constructor(props) {
+ constructor(props) {
     super(props);
+
     this.state = {
-        dishes: DISHES,
+      dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
     };
   }
 
 
   render() {
-    const HomePage = ()=>{
-      return(<Home/>);
-
+     const HomePage = () => {
+      return(
+          <Home 
+              dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+              promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+              leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+          />
+      );
     }
 
     return (
@@ -30,6 +45,7 @@ class Main extends Component {
          <Switch>
               <Route path='/home' component={HomePage} />
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+              <Route exact path='/contactus' component={Contact} />} />
               <Redirect to="/home" />
           </Switch>
         <Footer/>
