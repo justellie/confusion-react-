30fb0cf694a2bbfe043a4bcd95eb3dd3556510f0
Elio <elioruiz.en@gmail.com>
2019-02-13 12:26:57 -0400

    Segunda Tarea

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -11,6 +11,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 
 
 
@@ -55,6 +56,7 @@ class Main extends Component {
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
               <Route path='/menu/:dishId' component={DishWithId} />
               <Route exact path='/contactus' component={Contact} />} />
+              <Route path='/aboutus' component={()=> <About leaders={this.state.leaders}/>} />
               <Redirect to="/home" />
           </Switch>
         <Footer/>
